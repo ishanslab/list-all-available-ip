b2531593dfee2dd957a150187eb48800d67760ff
ishanslab <90043870+ishanslab@users.noreply.github.com>
2023-01-23 22:50:17 +0530

    Update list-all-available-ip.ps1

--- list-all-available-ip.ps1
+++ list-all-available-ip.ps1
@@ -3,9 +3,22 @@
         A runbook which gets all the Subnets, total IP addresses available and remaining IP addresses in each Subnet.
 
     .NOTES
-        AUTHOR: Ishan SHukla
+        AUTHOR: Ishan Shukla
         LASTEDIT: Jan 23, 2023
+
+    .PARAMETER Subid
+    Subscription ID for which the Subnets will be queried. Optional, if not provided, default Subscription will be selected. 
+
+    .PARAMETER Lessthan
+    Number for which you want to check the available IP addresses. 
 #>
+
+Param(
+    [Parameter(Mandatory=$false)]
+    [String]$Subid,
+    [Parameter(Mandatory=$false)]
+    [Int]$Lessthan
+)
 
 "Please enable appropriate RBAC permissions to the system identity of this automation account. Otherwise, the runbook may fail..."
 
@@ -13,6 +26,13 @@ try
 {
     "Logging in to Azure..."
     Connect-AzAccount -Identity
+    start-sleep -seconds 30
+    if($Subid -eq $null){
+       $Subid =  (Get-AzContext).Subscription.id 
+       Select-AzSubscription -SubscriptionId $Subid 
+    }
+
+    
 }
 catch {
     Write-Error -Message $_.Exception
@@ -21,11 +41,9 @@ catch {
 
 $vnetdet = @()
 
-$ResourceDetails = Get-AzResource #-ResourceGroupName $ResourceGroupName
+$getvnet =  Get-AzVirtualNetwork 
 
-$getvnet =  $ResourceDetails | Where-Object {$_.resourcetype -eq "Microsoft.Network/virtualNetworks"} | Get-AzVirtualNetwork 
-
-$subusage1  = $getvnet | foreach-Object {Get-AzVirtualNetworkUsageList -Name $_.Name -ResourceGroupName $_.ResourceGroupName }
+$subusage1 = $getvnet | foreach-Object {Get-AzVirtualNetworkUsageList -Name $_.Name -ResourceGroupName $_.ResourceGroupName }
 
 for ($v=0; $v -lt $getvnet.count; $v++){
 $getsubnet = $getvnet[$v] | Get-AzVirtualNetworkSubnetConfig 
@@ -42,12 +60,13 @@ for ($s=0; $s -lt $getsubnet.count; $s++){
             SubnetPrefix       = $($getsubnet[$s].AddressPrefix  -replace "{" -replace "}")
             SubnetIPsUsed      = $subusage.CurrentValue
             SubnetIPAvailable  = $subusage.Limit - $subusage.CurrentValue
+            PercentAvailable   = (($(($subusage.Limit - $subusage.CurrentValue)/$($subusage.Limit))*100))
             ResourceGroupName  = $getvnet[$v].ResourceGroupName
             SubscriptionName   = (Get-AzContext).Subscription.Name
     
 
         }
-        # VNET and Subnet details in a Resource Group.
+        # VNET and Subnet details.
         $vnetdet += New-Object PSObject  -Property $subnethash
 }
 }
@@ -56,6 +75,22 @@ for ($s=0; $s -lt $getsubnet.count; $s++){
 
 #$alert = $vnetdet |  Where-Object {$_.SubnetIPAvailable -lt '5'} 
   
-Write-Output ("Showing available IPs ")
+Write-Host "Showing available IPs " -ForegroundColor Green
 
-$vnetdet
+$vnetdet 
+
+If(!($Lessthan)){
+$alert = $vnetdet |  Where-Object {$_.PercentAvailable -lt '50'} 
+
+Write-Host  "Showing Subnets with less than 50% available IPs " -ForegroundColor Yellow
+
+$alert
+}
+
+Else{
+$alert = $vnetdet |  Where-Object {$_.PercentAvailable -lt $Lessthan} 
+
+Write-Host  "Showing Subnets with less than $($Lessthan)% available IPs " -ForegroundColor Yellow
+
+$alert
+}
